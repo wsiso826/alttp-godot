5bed1c7479af5004d5d8ed7854720a237499de26
wsiso826 <wsiso@live.com>
2023-12-09 11:11:20 -0500

    Fixed Walking Animation
    Fixed gutters in sprite sheet by using https://spritesheet-parser.vercel.app/. This fixed my animation sliding to the left.

--- player.gd
+++ player.gd
@@ -5,9 +5,9 @@ func _ready():
 	$AnimationPlayer.play("WalkDown");
 	
 func physics_process(delta):
-	if Input.is_action_just_pressed("ui_down"):
+	if Input.is_action_just_pressed("down"):
 		$AnimationPlayer.play("WalkDown");
-	if Input.is_action_just_pressed("ui_right"):
+	if Input.is_action_just_pressed("right"):
 		$AnimationPlayer.play("WalkRight");
 	if Input.is_action_just_pressed("ui_left"):
 		$AnimationPlayer.play("WalkLeft");
