bac0ea04e145f567c7905ba51313bda305a0934d
wsiso826 <wsiso@live.com>
2023-12-09 11:42:08 -0500

    Fixed button input issue for animation
    The player.gd script had an error. I corrected the error with help from Matt and now the button input shows the corresponding animation

--- player.gd
+++ player.gd
@@ -1,13 +1,11 @@
 extends CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	$AnimationPlayer.play("WalkDown");
-	
-func physics_process(delta):
+
+func _physics_process(delta): 
 	if Input.is_action_just_pressed("down"):
 		$AnimationPlayer.play("WalkDown");
-	if Input.is_action_just_pressed("right"):
+	elif Input.is_action_just_pressed("right"):
 		$AnimationPlayer.play("WalkRight");
 	if Input.is_action_just_pressed("ui_left"):
 		$AnimationPlayer.play("WalkLeft");
